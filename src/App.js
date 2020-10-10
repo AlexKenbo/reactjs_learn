533f1fd6703748a6e7b7ddfec1b579c21aaa2efa
Alex Vasiliev <alex.kenbo@yandex.ru>
2020-10-10 17:31:06 +0300

    Обертка для компонентов которые могут слать throw

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import errorMessage from './ErrorBoundary/ErrorBoundary';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 //import styled from 'styled-components';
 //import Radium, { StyleRoot } from 'radium'
@@ -71,12 +73,15 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
-              name={person.name}
-              age={person.age}
-              click={() => this.deletePersonHandler(index)}
-              key={person.id}
-              changed={(event) => this.nameChangeHandler(event, person.id)} />
+            return( 
+            <ErrorBoundary key={person.id}>
+              <Person
+                name={person.name}
+                age={person.age}
+                click={() => this.deletePersonHandler(index)}
+                key={person.id}
+                changed={(event) => this.nameChangeHandler(event, person.id)} />
+            </ErrorBoundary>)
           })}
         </div>
       );
