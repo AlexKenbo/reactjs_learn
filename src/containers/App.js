7fa81bc965d34e55f410783b376aab54548ddc21
Alex Vasiliev <alex.kenbo@yandex.ru>
2020-10-14 07:21:59 +0300

    High Order Components

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons'
 import Cockpit from '../components/Cockpit/Cockpit'
+import WithClass from '../hoc/WithClass';
 
 
 class App extends Component {
@@ -80,7 +81,7 @@ class App extends Component {
 
     return (
       //<StyleRoot>
-        <div className={classes.App}>
+        <WithClass classes={classes.App}>
           <button 
             onClick={() => {
               this.setState({showCockpit: !this.state.showCockpit})
@@ -93,7 +94,7 @@ class App extends Component {
             clicked={this.togglePersonsHandler}/>
             : null}
           {persons}
-        </div>
+        </WithClass>
     );
 
   }
